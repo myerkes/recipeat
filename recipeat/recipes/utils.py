d26d27a8844e30238ac8ec99f6366b87fbee4598
matt yerkes <mattyerkes23@gmail.com>
2024-09-20 11:27:52 -0400

    doc

--- recipeat/recipes/utils.py
+++ recipeat/recipes/utils.py
@@ -12,38 +12,50 @@ class RecipeScraper:
             self.scraper = scrape_html(html, org_url=url, wild_mode=True)
 
     def title(self):
+        # string
         return self.scraper.title()
 
     def ingredients(self):
+        # list of strings
         return self.scraper.ingredients()
     
     def parsed_ingredients(self):
+        # list of ParsedIngredient
         ingredients = self.scraper.ingredients()
         return [self.parse_ingredient(ingredient) for ingredient in ingredients]
 
     def parse_ingredient(self, ingredient_str):
+        # helper function to parse ingredients
         return parse_ingredient(ingredient_str)
 
     def ingredient_groups(self):
+        # list of IngredientGroup
         return self.scraper.ingredient_groups()
     
     def instructions(self):
+        # string
         return self.scraper.instructions()
     
     def instructions_list(self):
+        # list of strings
         return self.scraper.instructions_list()
     
     def to_json(self):
+        # json representation
         return self.scraper.to_json()
     
     def yields(self):
+        # string
         return self.scraper.yields()
 
     def total_time(self):
+        # int
         return self.scraper.total_time()
     
     def image(self):
+        # string - url
         return self.scraper.image()
     
     def links(self):
+        # list
         return self.scraper.links()
