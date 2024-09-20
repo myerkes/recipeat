59ee98801a8800efb143df59ff0e00cf9ca85f9d
matt yerkes <mattyerkes23@gmail.com>
2024-09-20 12:02:01 -0400

    add recipes model

--- recipeat/recipes/models.py
+++ recipeat/recipes/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    yields = models.CharField(max_length=255)
+    total_time = models.IntegerField()
+    image = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+    
+class Ingredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    comment = models.TextField()
+    name = models.CharField(max_length=255)
+    preparation = models.CharField(max_length=255)
+    purpose = models.CharField(max_length=255)
+    quantity = models.DecimalField(decimal_places=3,max_digits=7)
+    quantity_max = models.DecimalField(decimal_places=3,max_digits=7)
+    sentence = models.TextField()
+    size = models.CharField(max_length=255)
+    unit = models.CharField(max_length=255)
+    unit_text = models.CharField(max_length=255)
+
+class Instructions(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    step = models.IntegerField()
+    text = models.TextField()
